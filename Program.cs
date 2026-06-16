3a5c9e1497a692b065cc928bc2aa61b490b0eec5
3D AutoMate <info@pro-spect.co.nz>
2026-06-16 18:58:24 +1200

    Use accounting-only Xero OAuth scopes

--- Program.cs
+++ Program.cs
@@ -1268,7 +1268,7 @@ app.MapGet("/integrations/xero/connect-url", (string inspectorId) =>
         });
     }
 
-    var scopes = "openid profile email offline_access accounting.transactions accounting.contacts";
+    var scopes = "offline_access accounting.transactions accounting.contacts";
 
     var url =
         "https://login.xero.com/identity/connect/authorize" +
