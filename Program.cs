5f44a85b282343a91f53403975d23a199a922cdb
3D AutoMate <info@pro-spect.co.nz>
2026-06-21 19:34:38 +1200

    List SignNow terms folder with high limit

--- Program.cs
+++ Program.cs
@@ -7319,8 +7319,9 @@ static async Task<SignNowTemplateLookupResult> LookupSignNowTemplatesAsync(HttpC
     var signNowTermsParentId = "274f64b07a6e4d3bb14a0e7f6b51086d983d4240";
     var endpoints = new[]
     {
-        new SignNowTemplateEndpoint("terms_parent_documentsv2_parent_id", $"https://api.signnow.com/user/documentsv2?parent_id={Uri.EscapeDataString(signNowTermsParentId)}"),
-        new SignNowTemplateEndpoint("terms_parent_documentsv2_folder_id", $"https://api.signnow.com/user/documentsv2?folder_id={Uri.EscapeDataString(signNowTermsParentId)}"),
+        new SignNowTemplateEndpoint("terms_parent_documentsv2_parent_id", $"https://api.signnow.com/user/documentsv2?parent_id={Uri.EscapeDataString(signNowTermsParentId)}&limit=100"),
+        new SignNowTemplateEndpoint("terms_parent_documentsv2_parent_id_page", $"https://api.signnow.com/user/documentsv2?parent_id={Uri.EscapeDataString(signNowTermsParentId)}&page=1&limit=100"),
+        new SignNowTemplateEndpoint("terms_parent_documentsv2_folder_id", $"https://api.signnow.com/user/documentsv2?folder_id={Uri.EscapeDataString(signNowTermsParentId)}&limit=100"),
         new SignNowTemplateEndpoint("template", "https://api.signnow.com/template"),
         new SignNowTemplateEndpoint("user_documents_template", "https://api.signnow.com/user/documents?type=template"),
         new SignNowTemplateEndpoint("user_documentsv2_template", "https://api.signnow.com/user/documentsv2?type=template")
@@ -7369,8 +7370,8 @@ static async Task<SignNowTemplateLookupResult> LookupSignNowTemplatesAsync(HttpC
         }
     }
 
-    var preferredTemplates = templates.Any(template => string.Equals(template.SourceType, "terms_parent_documentsv2_parent_id", StringComparison.OrdinalIgnoreCase))
-        ? templates.Where(template => string.Equals(template.SourceType, "terms_parent_documentsv2_parent_id", StringComparison.OrdinalIgnoreCase))
+    var preferredTemplates = templates.Any(template => template.SourceType.StartsWith("terms_parent_documentsv2_parent_id", StringComparison.OrdinalIgnoreCase))
+        ? templates.Where(template => template.SourceType.StartsWith("terms_parent_documentsv2_parent_id", StringComparison.OrdinalIgnoreCase))
         : templates.Any(template => string.Equals(template.SourceType, "terms_parent_documentsv2_folder_id", StringComparison.OrdinalIgnoreCase))
             ? templates.Where(template => string.Equals(template.SourceType, "terms_parent_documentsv2_folder_id", StringComparison.OrdinalIgnoreCase))
             : templates.Any(template => string.Equals(template.SourceType, "user_documentsv2_template", StringComparison.OrdinalIgnoreCase))
